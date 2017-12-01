2628cb669615f1df5a5e0fc09e3ab6c6f0860f2a
Arkonel <34172672+Arkonel@users.noreply.github.com>
2017-12-01 14:09:33 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+ 
+bot.on('message', (message) => {
+    if(message.content == "Fairy-Bot"){
+        message.channel.send('Hi, im a bot heres a list of my current commands:\n!macro');
+    }   
+    if(message.content.toLowerCase() == "poke"){
+        message.channel.send('ouch');
+    }
+     
+ 
+    if(new RegExp('^!macro').exec(message.content) ){
+ 
+        var data = message.content.toLowerCase().split(' ');
+        if(data.length == 1) {
+            message.channel.send(' \nList of commands for macro:\nWeaponGlow');
+        } else switch (data[1]) {
+            case 'weaponglow':        
+            message.channel.send("Weapon glow:\n/bm\n/sit");
+            break;
+            case 'protect':
+            message.channel.send("Protect: (in progress)");
+        }
+ 
+    }
+});
+ 
+bot.login(process.env.BOT_TOKEN);
