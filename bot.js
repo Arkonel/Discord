1b19194cc2c64e81338f3be2d58dc7d8060b22d2
Arkonel <34172672+Arkonel@users.noreply.github.com>
2017-12-02 10:47:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,24 +4,13 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     
     if(message.content == "Fairy-Bot"){
-        message.channel.send('Hi, im a bot heres a list of my current commands:\n!macro\n!rps');
+        message.channel.send('Hi, im a bot heres a list of my current commands:\n!macro');
     }  
     if(message.content.toLowerCase() == "poke"){
         message.channel.send('ouchy');
     
     }
-    if(message.content == "!rps"){
-        message.channel.send('Rock Paper Scissors! Type !rps (:fist:/:v:/:raised_hand:) to play!');
-    }
-    if(new RegExp('^!rps :fist:').exec(message.content)){
-        message.channel.send(':raised_hand: Paper beats rock! I win!');
-    }   
-    if(new RegExp('^!rps :v:').exec(message.content)){
-        message.channel.send(':fist: Rock beats scissors! I win!');
-    }  
-    if(new RegExp('^!rps :raised_hand:').exec(message.content)){
-        message.channel.send(':fist: Paper beats rock! You win....');
-    }  
+    
     if(new RegExp('^!macro').exec(message.content)){
  
         var data = message.content.toLowerCase().split(' ');
