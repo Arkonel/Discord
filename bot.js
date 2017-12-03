de02c39ffb463dccb805d280278a20827c8dd978
Arkonel <34172672+Arkonel@users.noreply.github.com>
2017-12-02 16:58:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,11 @@ bot.on('message', (message) => {
         message.channel.send('Hi, im a bot heres a list of my current commands:\n!macro');
     }  
     if(message.content.toLowerCase() == "poke"){
-        var rand = Math.floor(Math.random())*10+1;
-        if (rand <7) {
+        var rand = Math.random();
+        if (rand < .7) {
          message.channel.send('Owie! What did I ever do to you?');
-    } else {message.channel.send('I poke you back!');
+    } else {
+         message.channel.send('I poke you back!');
     }
     
     if(new RegExp('^!macro').exec(message.content)){
