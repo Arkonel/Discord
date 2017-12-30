20e232a2ba1c9709ec291f552c2e59b84e6e90b1
Arkonel <34172672+Arkonel@users.noreply.github.com>
2017-12-30 14:06:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ bot.on('message', (message) => {
         if (!message.guild) return;
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voiceChannel) {
-          message.member.voiceChannel.join();
+          voiceChannel.join();
           message.reply('Attempting to join ' + message.member.voiceChannel.toString());
         } else {
           message.reply('You need to join a voice channel first!');
